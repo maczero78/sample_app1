0aba86edf030988d4f1a7eec9df20ef86221d7df
U-Gate\maczero <maczero78@msn.com>
2011-11-20 16:51:38 -0500

    adding a base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+# Ruby base title per page basis
+  def title 
+   base_title ="Ruby on rails Sample app"
+   if @title.nil?
+    base-title
+	else
+      "#{base_title} | #{@title}"
+	  end 
+	
+  end
+
+
+
 end
